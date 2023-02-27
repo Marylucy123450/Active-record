272ea29f435688bcef1fafb8a23d36acb707f8ed
Marylucy <marylucyatienoomenda.gmail.com>
2023-02-27 12:10:00 +0300

    coding

---
+++ db/migrate/20230227075923_create_review.rb
@@ -0,0 +1,10 @@
+class CreateReview < ActiveRecord::Migration[6.1]
+  def change
+    create_table review do /t/
+      t.integer :star_rating
+      t.integer :user_id
+      t.integer :product_id
+      t.integer :comment
+    end
+  end
+end
